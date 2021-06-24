939ed7361af86400e2ab9fd478c0912b23322ac1
Apeksha Haveri <apeksha.haveri@ecanarys.com>
2021-06-24 15:06:13 +0530

    trial

--- .github/actions/issue-maker/index.js
+++ .github/actions/issue-maker/index.js
@@ -8,10 +8,10 @@ async function run() {
     const token = core.getInput("repo-token");
 
     const octokit = new github.getOctokit(token);
-    const context = github.context.repo;
+
     const newIssue = await octokit.rest.issues.create({
-      repo: context.repo,
-      owner: context.owner,
+      repo: github.context.repo.repo,
+      owner: github.context.repo.owner,
       title: issueTitle,
       body: jokeBody
     });
